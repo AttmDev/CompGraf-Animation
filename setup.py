bea2033e2d27c346e92da9e50c6ab9767242e549
Lucas <lucasmottacp@gmail.com>
2019-09-09 19:54:10 -0300

    -cod organizado

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='Trabalho de CG',
+    version='0.0.1',
+    url='https://github.com/Attmic/CompGraf-Animation',
+    author='[nome_do_grupo????]',
+    description=''
+)
